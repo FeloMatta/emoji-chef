626df0bbfa2dcce669c38cdca5a8634ae6972de5
FeloMatta <mattafelo@gmail.com>
2023-08-25 23:25:25 +0200

    aggiunto OPENAI per poter mandare le richieste a gpt in formato JSON

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
+const OPENAI = {
+    // inserisci la tua chiave
+    API_KEY: '',
+    API_BASE_URL: 'https://api.openai.com/v1',
+    CHAT_ENDPOINT: '/chat/completions',
+    IMAGE_ENDPOINT: '/images/generations',  
+};
+
 const ingredients = document.querySelectorAll('.ingredient');
 const bowlSlots = document.querySelectorAll('.bowl-slot');
 const cookButton = document.querySelector('#cook-button');
@@ -35,6 +43,47 @@ function addIngredient(ingredient){
     }
 }
 
-function createRecipe() {
+async function createRecipe() {
     loading.classList.remove('hidden');
+
+    const prompt = `\
+Crea una ricetta con questi ingredienti: ${bowl.join(', ')}.
+La ricetta deve essere facile e con un titolo creativo e divertente.
+Le tue risposte sono solo in formato JSON come questo esempio:
+
+###
+
+{
+    "titolo": "Titolo ricetta",
+    "ingredienti": "1 uovo e 1 pomodoro",
+    "istruzioni": "mescola gli ingredienti e metti in forno"
+}
+
+###`;
+
+    const recipeResponse = await makeRequest(OPENAI.CHAT_ENDPOINT, {
+        model: 'gpt-3.5-turbo',
+        messages: [{
+            role: 'user',
+            content: prompt,
+        }]
+    })
+
+    const recipe = JSON.parse(recipeResponse.choices[0].message.content);
+
+    
+}
+
+async function makeRequest(endpoint, payload) {
+    const response = await fetch(OPENAI.API_BASE_URL + endpoint, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${OPENAI.API_KEY}`
+        },
+        body: JSON.stringify(payload)
+    });
+
+    const json = await response.json();
+    return json;
 }
